1a62dcdbab23baafc122488a35e8e238fd5d99bc
Bharat-00 <bharat.trivedi_cs22@gla.ac.in>
2026-05-05 17:02:31 +0530

    feat: auth service dockerized with gateway and eureka

---
+++ auth-service/src/main/java/com/learnova/auth/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.learnova.auth.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
